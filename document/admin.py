695a220e2c36b861157c6f5bddc68fa49a15e0d8
Rahidul Islam <rahiseli@outlook.com>
2023-07-19 21:14:21 +0600

    document model is updated in admin panel

--- document/admin.py
+++ document/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Document
 # Register your models here.
-admin.site.register(Document)
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'file', 'file_format', 'created_at']
+    list_filter = ['owner','file_format', 'created_at']
