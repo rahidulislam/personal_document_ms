dee8591b112a8b089906f04f12346095dd920a4e
Rahidul Islam <rahiseli@outlook.com>
2023-07-19 21:08:27 +0600

    refresh token api is added

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserRegistrationView, UserLoginView
 
 app_name = 'account'
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user_registration'),
-    path('login/', UserLoginView.as_view(), name='user_login'),
+    path('token/', UserLoginView.as_view(), name='user_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
